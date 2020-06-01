a053af1530326b74b1f4b11ea29de9cc59e018a8
Jamezjaz <jamezjaz@gmail.com>
2020-06-01 16:37:45 -0500

    Adds rspec for 'display board'

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,6 +1,7 @@
 require './lib/board'
 
 describe 'PlayTicTacToe' do
+
   describe "#check_o_x" do
     game = PlayTicTacToe.new
     it "returns true if X or O exists" do
@@ -9,6 +10,14 @@ describe 'PlayTicTacToe' do
 
     it "returns false if not X or O" do
       expect(game.check_o_x("GFG")).to be (false)
+    end
+  end
+
+  describe "#display_my_board" do
+    game = PlayTicTacToe.new
+    
+    it "returns array of strings" do
+      expect(game.display_my_board).to be_an_instance_of(Array)
     end
   end
 end
