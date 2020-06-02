b388e8c645aebd590f6dd7da4959a8cf309b5600
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2020-06-02 22:08:09 +0300

    modifies main.rb file to run load it when call

--- bin/main.rb
+++ bin/main.rb
@@ -1,22 +1,22 @@
 require_relative '../lib/board'
 
-puts "Player1's name"
-@player1 = gets.chomp
+# puts "Player1's name"
+# @player1 = gets.chomp
 
-while @player1.split.empty?
-  puts "Please insert player1's name! can't be blank"
-  @player1 = gets.chomp
-end
-puts "Playe2's name"
-@player2 = gets.chomp
-while @player2.split.empty? || @player2 == @player1
-  puts "insert Player2 name and it should be differnt from player1's"
-  @player2 = gets.chomp
-end
-@game_on_going = true
-@current_player = 'X'
+# while @player1.split.empty?
+#   puts "Please insert player1's name! can't be blank"
+#   @player1 = gets.chomp
+# end
+# puts "Playe2's name"
+# @player2 = gets.chomp
+# while @player2.split.empty? || @player2 == @player1
+#   puts "insert Player2 name and it should be differnt from player1's"
+#   @player2 = gets.chomp
+# end
+# @game_on_going = true
+# @current_player = 'X'
 
-@my_game = PlayTicTacToe.new
+# @my_game = PlayTicTacToe.new
 
 def display_board
   puts @my_game.display_my_board[0] + '|' + @my_game.display_my_board[1] + '|' + @my_game.display_my_board[2]
@@ -44,13 +44,15 @@ def players_turn
   @my_game.display_my_board[turn] = @current_player
 end
 
-def flip_player
-  if @current_player == 'X'
-    @current_player = 'O'
-    puts "It's #{@player2}'s turn to play"
+def flip_player(current_player, player1, player2)
+  if current_player == 'X'
+    # current_player = 'O'
+    puts "It's #{player2}'s turn to play"
+    return 'O'
   else
-    @current_player = 'X'
-    puts "It's #{@player1}'s turn to play"
+    # current_player = 'X'
+    puts "It's #{player1}'s turn to play"
+    return 'X'
   end
 end
 
@@ -67,9 +69,9 @@ def current_player
   end
 end
 
-def check_tie
-  @game_on_going = false if @my_game.if_tie
-  puts "It's a tie. Game over!" if @my_game.if_tie && !@my_game.check_win
+def check_tie(game_on_going, my_game)
+  game_on_going = false if my_game.if_tie
+  puts "It's a tie. Game over!" if my_game.if_tie && !my_game.check_win
 end
 
 def play_game
@@ -78,10 +80,30 @@ def play_game
   while @game_on_going
     players_turn
     display_board
-    check_tie
+    check_tie(@game_on_going, @my_game)
     check_win
-    flip_player if @game_on_going
+    @current_player =  flip_player(@current_player, @player1, @player2) if @game_on_going
   end
 end
 
-play_game
+if __FILE__ == $0
+  puts "Player1's name"
+@player1 = gets.chomp
+
+while @player1.split.empty?
+  puts "Please insert player1's name! can't be blank"
+  @player1 = gets.chomp
+end
+puts "Playe2's name"
+@player2 = gets.chomp
+while @player2.split.empty? || @player2 == @player1
+  puts "insert Player2 name and it should be differnt from player1's"
+  @player2 = gets.chomp
+end
+@game_on_going = true
+@current_player = 'X'
+
+@my_game = PlayTicTacToe.new
+
+  play_game
+end
